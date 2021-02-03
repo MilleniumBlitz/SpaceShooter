ff88b86af436774a420e91ab302759c446f73899
MilleniumBlitz <xavier.lafficher@gmail.com>
2021-02-03 19:13:53 +0100

    Improve EnemySpawner locations

--- Levels/Base/BaseLevel.gd
+++ Levels/Base/BaseLevel.gd
@@ -3,7 +3,6 @@ extends Node2D
 func _on_IntroTimer_timeout():
 	LevelManager.level_started = true
 	
-	
 	$EnemySpawner.waves = LevelManager.waves
 	$EnemySpawner.start()
 	LevelManager.connect("player_hit", $CanvasLayer/HealthUI, "player_hit")
